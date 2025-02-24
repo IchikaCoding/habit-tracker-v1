b7b580241a214186a2888a9fe058334facf85c4f
IchikaCoding <IchikaCoding@users.noreply.github.com>
2025-02-24 14:58:07 +0900

    Initial commit: 現在のコードをアップロード

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  authDomain: "habit-tracker-v1-d0abd.firebaseapp.com",
+  projectId: "habit-tracker-v1-d0abd",
+  storageBucket: "habit-tracker-v1-d0abd.firebasestorage.app",
+  messagingSenderId: "101077910727",
+  appId: "1:101077910727:web:7e703cb0bc84534c852ae2",
+  measurementId: "G-PGSXFF4RJS",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { db, app, analytics };
